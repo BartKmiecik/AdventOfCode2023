1cc79e7fae8c6e08fd55f78fcc73e4923e2e6e3e
Bart <kmiecikjob@gmail.com>
2023-12-22 12:38:07 +0100

    Day 6 exr 2

---
+++ day6_exr2.py
@@ -0,0 +1,27 @@
+import re
+
+patern = f'([0-9]+)'
+data = open('day6', 'r')
+
+times = re.findall(patern, data.readline())
+records = re.findall(patern, data.readline())
+total_time = ''
+for t in times:
+    total_time += t
+total_record = ''
+for r in records:
+    total_record += r
+
+
+single_result = 0
+for i in range(int(total_time)):
+    distance = i * (int(total_time) - i)
+    if distance > int(total_record):
+        single_result += 1
+
+
+print(single_result)
+# print(total_time)
+# print(total_record)
+
+data.close()

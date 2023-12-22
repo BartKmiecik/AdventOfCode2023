0d402a0871b61a5e87229d1ee22c8091b7f3f2b8
Bart <kmiecikjob@gmail.com>
2023-12-22 12:29:58 +0100

    Day 6 exr 1

---
+++ day5_exr2.py
@@ -0,0 +1,56 @@
+import re
+import numpy as np
+from day5_supportingClass import SeedToPlant
+
+
+patern = f'([0-9]+)'
+groups_names = ['seeds', 'soil', 'fertilizer', 'water', 'light', 'temperature', 'humidity', 'location']
+groups = {}
+counter = 0
+data = open('day5', 'r')
+next = data.readline()
+groups[groups_names[counter]] = re.findall(patern, next)
+seeds = []
+seeds_ranges = int(len(groups['seeds'])/2)
+for seed in range(int(groups['seeds'][0]), int(groups['seeds'][0]) + int(groups['seeds'][1])):
+    print(seed)
+
+        # seeds.append(SeedToPlant(int(seed)))
+# for seed in groups['seeds']:
+#     seeds.append(SeedToPlant(int(seed)))
+
+# print(len(seeds))
+
+# next = data.readline()
+# while next:
+#     numbs = re.findall(patern, next)
+#     if not numbs and groups_names[counter] in groups:
+#         counter += 1
+#     elif groups_names[counter] in groups:
+#         groups[groups_names[counter]] += numbs
+#     else:
+#         groups[groups_names[counter]] = numbs
+#     next = data.readline()
+#
+# for i in range(1,len(groups_names)):
+#     if i == 0:
+#         continue
+#     triplets = int((len(groups[groups_names[i]])+1)/3)
+#     for j in range(triplets):
+#         dest = int(groups[groups_names[i]][j*3])
+#         start = int(groups[groups_names[i]][j * 3 + 1])
+#         end = int(groups[groups_names[i]][j * 3 + 2])
+#         for seed in seeds:
+#             value = seed.return_parameter(groups_names[i])
+#             if value in range(start, start + end):
+#                 seed.change_parameter(groups_names[i], value - (start - dest), True)
+#
+#
+# max = (1000000000, 1000000000)
+# for seed in seeds:
+#     # print(seed)
+#     if seed.location < max[1]:
+#         max = (seed.seeds, seed.location)
+#
+# print(max)
+# data.close()
---
+++ day6_exr1.py
@@ -0,0 +1,22 @@
+import re
+
+patern = f'([0-9]+)'
+data = open('day6', 'r')
+
+times = re.findall(patern, data.readline())
+records = re.findall(patern, data.readline())
+
+result = 1
+for time in range(len(times)):
+    single_result = 0
+    for i in range(int(times[time])):
+        distance = i * (int(times[time])-i)
+        if distance > int(records[time]):
+            single_result += 1
+    result *= single_result
+
+print(result)
+# print(times)
+# print(records)
+
+data.close()

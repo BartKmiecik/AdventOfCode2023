f42cf150afa02cb9074dc7a4e685d61eaaa56eb1
Bart <kmiecikjob@gmail.com>
2023-12-27 12:32:06 +0100

    Day 9 exr 1

---
+++ code/day9_exr1.py
@@ -0,0 +1,26 @@
+def calculate_diff(list_of_numbs:list):
+    new_list = []
+    should_repeat = False
+    for n in range(len(list_of_numbs) - 1):
+        var = int(list_of_numbs[n+1]) - int(list_of_numbs[n])
+        new_list.append(var)
+        if not should_repeat and var != 0:
+            should_repeat = True
+    return new_list, should_repeat
+
+data = open('../text/day9', 'r')
+
+next = data.readline().split()
+result = 0
+
+while next:
+    # print(next)
+    result += int(next[-1])
+    diff, repeat = calculate_diff(next)
+    while repeat:
+        result += int(diff[-1])
+        diff, repeat = calculate_diff(diff)
+    next = data.readline().split()
+
+print(result)
+data.close()
